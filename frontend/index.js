c5e69eae920b3a8b8f08785e0fbad8ca6599c2d6
Varshitha <kusivarshitha23@gmail.com>
2026-08-13 16:46:20 +0530

    toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,29 @@
-alert("welcome to the bootcamp");
+
+
+var users=[
+    {
+        "name":"john doe",
+        "gender":"male",
+        "image":"john.png"
+    },
+    {
+        "name":"jane doe",
+        "gender":"female",
+        "image":"",
+    }
+]
+
+var curId = 0;
+
+function toggleUser(){
+    curId = (curId+1)% 2;
+
+    var userName= document.getElementById("user-name");
+    var userGender=document.getElementById("user-gender");
+    var userImage=document.getElementById("user-Image");
+
+    userName.innerHTML = users[curId].name;
+    userGender.innerHTML = users[curId].gender;
+    userImage.src=user[curId].image;
+
+}
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express();
 const port= process.env.PORT || 8080;
 app.use(express.static("frontend"));
 app.listen(port, function(){
-    console.log("server running on https://localhost:"+port);
+    console.log("server running on http://localhost:"+port);
 });
